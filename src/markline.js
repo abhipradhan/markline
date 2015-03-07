1263ca815875bae38b945a67ddb2029ca6ace607
curioustechizen <techie.curious@gmail.com>
2015-03-07 15:37:09 +0530

    Initial work on adding input textarea for editing markdown

--- src/markline.js
+++ src/markline.js
@@ -2,14 +2,40 @@
 (function () {
   'use strict';
 
-  function markline() {
-    var reader = new commonmark.Parser(),
-      writer = new commonmark.HtmlRenderer(),
-      parsed = reader.parse(document.getElementById("markdown-content").textContent),
-      result = writer.render(parsed);
-
-    document.getElementById("doc-target").innerHTML = result;
+  function initMarkline(){
+    var btnEdit = document.getElementById("btn-edit");
+    btnEdit.addEventListener("click", edit);
+    
+    var btnRender = document.getElementById("btn-render");
+    btnRender.addEventListener("click", render);
+    
+    render();
   }
   
-  document.addEventListener("DOMContentLoaded", markline);
+  function render(event) {
+    var reader = new commonmark.Parser(),
+      writer = new commonmark.HtmlRenderer(),
+      inputMarkdown = document.getElementById("input-markdown").value,
+      parsed = reader.parse(inputMarkdown),
+      result = writer.render(parsed);
+
+    if(event) {
+      event.preventDefault();
+    }
+    document.getElementById("doc-target").innerHTML = result;
+    setVisibility(document.getElementById("markline-edit"), false);
+    setVisibility(document.getElementById("markline-render"), true);
+  }
+  
+  function edit(event) {
+    event.preventDefault();
+    setVisibility(document.getElementById("markline-edit"), true);
+    setVisibility(document.getElementById("markline-render"), false);
+  }
+  
+  function setVisibility(node, show) {
+    node.style.display = show ? 'block' : 'none';
+  }
+  
+  document.addEventListener("DOMContentLoaded", initMarkline);
 }());
